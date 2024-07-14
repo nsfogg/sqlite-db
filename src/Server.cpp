1b27115205e193e862994302b3425507b3a38c6a
Nick Fogg <nsfogg@ncsu.edu>
2024-07-14 18:31:33 -0500

    server first update

--- src/Server.cpp
+++ src/Server.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
     std::cerr << std::unitbuf;
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here" << std::endl;
+//    std::cout << "Logs from your program will appear here" << std::endl;
 
     if (argc != 3) {
         std::cerr << "Expected two arguments" << std::endl;
@@ -25,15 +25,14 @@ int main(int argc, char* argv[]) {
             return 1;
         }
 
-        // Uncomment this to pass the first stage
-        // database_file.seekg(16);  // Skip the first 16 bytes of the header
-        // 
-        // char buffer[2];
-        // database_file.read(buffer, 2);
-        // 
-        // unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-        // 
-        // std::cout << "database page size: " << page_size << std::endl;
+         database_file.seekg(16);  // Skip the first 16 bytes of the header
+         
+         char buffer[2];
+         database_file.read(buffer, 2);
+         
+         unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+         
+         std::cout << "database page size: " << page_size << std::endl;
     }
 
     return 0;
